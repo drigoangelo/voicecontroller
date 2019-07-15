629f81dd196e0d704abe4daced5f292a03144443
Rodrigo Angelo <rangelo@synvata.com>
2019-07-15 13:26:10 -0300

    Adicionando log

--- src/main/java/br/ufu/ileel/voicecontroller/VoiceService.java
+++ src/main/java/br/ufu/ileel/voicecontroller/VoiceService.java
@@ -2,6 +2,8 @@ package br.ufu.ileel.voicecontroller;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,17 +14,32 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class VoiceService {
 	
-	@GetMapping("/hello")
+	private Logger logger;
+
+	@Autowired
+    public VoiceService(Logger logger) {
+        this.logger = logger;
+    }
+	
+	@GetMapping("/ping")
 	public String hello() {
-		return "hello world";
+		logger.info("ping");
+		return "pong";
 	}
 
 	@PostMapping("/voicechat")
-    public byte[] uploadMultipartFile(@RequestParam("uploadfile") MultipartFile file) {
-        try {
+    public byte[] talk(@RequestParam("uploadfile") MultipartFile file) {
+		logger.info("talk");
+		try {
         	RestTemplate restTemplate = new RestTemplate();
         	
-        	restTemplate.postForEntity("lab-deepspeech:8080", file, Object.class);
+        	Object response = restTemplate.postForEntity("lab-deepspeech:8080", file, Object.class);
+        	
+        	String respChat = response.toString();
+        	logger.info("respChat: {}", respChat);
+        	
+        	Object responseTacotron = restTemplate.postForEntity("lab-tacotron:80", "bed beach sink", Object.class);
+        	logger.info("responseTacotron: {}", responseTacotron.toString());
         	
 			return file.getBytes();
 		} catch (IOException e) {
